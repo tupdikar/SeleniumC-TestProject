4c9e7145a173ea298073ad1924e6a08fd3fc62a2
tupdikar <tupdikar.s@gmail.com>
2024-11-10 22:39:26 +0530

    Add project files.

---
+++ FlipKartPracticeProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlipKartPracticeProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlipKartPracticeProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35a45705-cd22-4690-a17a-551d8266b55c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PracticeProject/UnitTest1.vb
@@ -0,0 +1,20 @@
+Imports NUnit.Framework
+
+Namespace PracticeProject
+
+    Public Class Tests
+
+        <SetUp>
+        Public Sub Setup()
+        End Sub
+
+        <Test>
+        Public Sub Test1()
+            Assert.Pass()
+        End Sub
+
+    End Class
+
+End Namespace
+
+
